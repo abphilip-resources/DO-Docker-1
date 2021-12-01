18aaaafb2ebf2c4ad93b1db428b31a3fd299ba3b
allenalvin333 <allenalvin333@gmail.com>
2021-12-01 13:59:55 +0530

    programs

--- 2/1.py
+++ 2/1.py
@@ -1,7 +1,4 @@
-import pandas
-import seaborn
-import matplotlib
-import numpy
+import pandas, seaborn, matplotlib, numpy
 
 print(f'Numpy: {numpy.__version__}')
 print(f'Matplotlib: {matplotlib.__version__}')
